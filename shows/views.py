98c98aad7e452d0c33c194f90866f8bec89c5b48
Viktor.Velikov <vvelikov1988@gmail.com>
2020-07-19 14:34:26 -0700

    final

--- shows/views.py
+++ shows/views.py
@@ -5,8 +5,8 @@ def index(request):
     return render(request, 'index.html')
 
 def create(request):
-    Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date = request.POST['reldate'], description = request.POST['desc'])
-    return redirect ('/')
+    new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date = request.POST['reldate'], description = request.POST['desc'])
+    return redirect (f'/shows/{new_show.id}')
 
 def showdetails(request, show_id):
     show = Shows.objects.get(id=show_id)
@@ -14,6 +14,7 @@ def showdetails(request, show_id):
         "show": show
     }
     return render(request, 'showdetails.html', context)
+
 def allshows(request):
     shows = Shows.objects.all()
     context = {
@@ -21,15 +22,26 @@ def allshows(request):
     }
     return render(request, 'shows.html', context)
 
-def editshow(request, show_id):
-    Shows.objects.update(title=request.POST['title'], network=request.POST['network'], release_date = request.POST['reldate'], description = request.POST['desc'])
+def update(request, show_id):
+    show_update = Shows.objects.get(id=show_id)
+    show_update.title=request.POST['title']
+    show_update.network=request.POST['network']
+    show_update.release_date = request.POST['reldate'] 
+    show_update.description = request.POST['desc']
+    show_update.save()
     return redirect (f'/shows/{show_id}')
 
-def update(request, show_id):
-    return render(request, 'index.hmtl')
+def editshow(request, show_id):
+    show = Shows.objects.get(id=show_id)
+    context = {
+        "show": show
+    }
+    return render(request, 'editshow.html', context)
 
 def destroy(request, show_id):
-    return render(request, 'index.hmtl')
+    show_delete = Shows.objects.get(id = show_id)
+    show_delete.delete()
+    return redirect('/shows')
 
 
 
