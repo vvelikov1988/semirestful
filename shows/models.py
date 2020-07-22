80690b246e01ba5361b2c4aad22d513b697f2a28
Viktor.Velikov <vvelikov1988@gmail.com>
2020-07-21 19:56:23 -0700

    validations

--- shows/models.py
+++ shows/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from datetime import datetime
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, formData):
+        errors = {}
+        if len(formData['title']) < 2:
+            errors["title"] = "Show title should be at least 3 characters"
+        if len(formData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+        if formData['desc'] != '' and len(formData['desc']) < 10:
+            errors['desc'] = "Description should be at least 10 characters"
+        if datetime.strptime(formData['reldate'], '%Y-%m-%d') > datetime.now():
+            errors['reldate'] = "Release Date should be in the past"
+        return errors
 
 class Shows(models.Model):
     title = models.CharField(max_length=255)
@@ -7,4 +22,5 @@ class Shows(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
--- shows/views.py
+++ shows/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import Shows
 
 def index(request):
     return render(request, 'index.html')
 
 def create(request):
-    new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date = request.POST['reldate'], description = request.POST['desc'])
+    errors = Shows.objects.basic_validator(request.POST)
+    if errors:
+        for (key, value) in errors.items():
+            messages.error(request, value)
+            return redirect('/shows/new')
+    else:
+        new_show = Shows.objects.create(
+            title=request.POST['title'], 
+            network=request.POST['network'], 
+            release_date = request.POST['reldate'], 
+            description = request.POST['desc'])
+        
     return redirect (f'/shows/{new_show.id}')
 
 def showdetails(request, show_id):
@@ -23,15 +35,29 @@ def allshows(request):
     return render(request, 'shows.html', context)
 
 def update(request, show_id):
-    show_update = Shows.objects.get(id=show_id)
-    show_update.title=request.POST['title']
-    show_update.network=request.POST['network']
-    show_update.release_date = request.POST['reldate'] 
-    show_update.description = request.POST['desc']
-    show_update.save()
-    return redirect (f'/shows/{show_id}')
+        # pass the post data to the method we wrote and save the response in a variable called errors
+    errors = Shows.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        show_update = Shows.objects.get(id=show_id)
+        show_update.title=request.POST['title']
+        show_update.network=request.POST['network']
+        show_update.release_date = request.POST['reldate'] 
+        show_update.description = request.POST['desc']
+        show_update.save()
+        messages.success(request, "Show successfully updated")
+        # redirect to a success route
+        return redirect (f'/shows/{show_id}')
+
 
 def editshow(request, show_id):
+    
     show = Shows.objects.get(id=show_id)
     context = {
         "show": show
